5a190f6a1c4e10030437d24c2eaecc0ce7dc979e
Juan Delgado <jdelgadop@taxislibres.com.co>
2016-01-07 09:18:30 -0500

    cambio en la rama1

--- RepoRamas/ejbModule/paquete1/ArchivoRama2.java
+++ RepoRamas/ejbModule/paquete1/ArchivoRama2.java
@@ -2,6 +2,6 @@ package paquete1;
 
 public class ArchivoRama2 {
 	public static void main(String args[]) {
-		System.out.println("Cambio heche en la rama 2");
+		System.out.println("Cambio hecho en la rama 1");
 	}
 }
